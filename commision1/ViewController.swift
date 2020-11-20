f2fcb95f0780649b9deeac3cfd34b1039b6ca88e
romanthegg <rzadorskyy3454@stu.d214.org>
2020-11-20 16:14:33 -0500

    added calculator function

--- commision1/ViewController.swift
+++ commision1/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var commissionPayTextField: UITextField!
+    @IBOutlet weak var totalPayLabel: UILabel!
+    
+    let basePay = 500
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButton(_ sender: Any) {
+        let commissionPayString = commissionPayTextField.text!
+        let commissionPay = Double(commissionPayString)!
+        
+        let totalPay = Double(basePay) + commissionPay
+        
+        totalPayLabel.text = "\(totalPay)"
+        
+          let totalOutput = String(format: "%.2f", totalPay)
+        
+        totalPayLabel.text = "$\(totalOutput)"
+    }
+    
 }
 
